11f7ea60497e877e1359ed32b0a9e3906834c68b
DariiaVyshenska <dariia.vyshenska@gmail.com>
2023-12-22 09:45:23 -0800

    docs: added readme file

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('./config/mongo');
+const cors = require("cors");
+require("./config/mongo");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-const recordRoutes = require('./routes/record_routes');
-const hookRoutes = require('./routes/hook_routes');
+const recordRoutes = require("./routes/record_routes");
+const hookRoutes = require("./routes/hook_routes");
 
-app.use('/api', recordRoutes);       
-app.use('/', hookRoutes);
+app.use("/api", recordRoutes);
+app.use("/", hookRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App is up and running on ${PORT}`);
